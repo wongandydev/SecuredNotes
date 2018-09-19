fa72f9058ba2c2e9a8cc1ee762c5967b2e8cbc95
wongandydev <wongandy97@gmail.com>
2018-09-18 20:24:36 -0400

    move readJSON to a singleton so it can be used in both VC for DRY code

---
+++ GoingToJapan/AddNotesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddNotesViewController.swift
+//  GoingToJapan
+//
+//  Created by Andy Wong on 9/17/18.
+//  Copyright © 2018 Andy Wong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddNotesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GoingToJapan/FakeAPIManager.swift
@@ -0,0 +1,42 @@
+//
+//  FakeAPIManager.swift
+//  GoingToJapan
+//
+//  Created by Andy Wong on 9/18/18.
+//  Copyright © 2018 Andy Wong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FakeAPIManager {
+    static let sharedInstance = FakeAPIManager()
+    
+    func readJSON() -> [DayPassObject]{
+        var daysArr: [DayPassObject] = []
+        
+        if let filePath = Bundle.main.path(forResource: "fakeData", ofType: "json") {
+            do {
+                let json = try Data(contentsOf: URL(fileURLWithPath: filePath), options: .mappedIfSafe)
+                let jsonResults = try JSONSerialization.jsonObject(with: json, options: .mutableLeaves)
+                if let jsonResults = jsonResults as? [AnyObject] {
+                    for result in jsonResults {
+                        if let date = result["date"] as? String,
+                            let password = result["password"] as? String,
+                            let letter = result["note"] as? String{
+                            daysArr.append(DayPassObject(date: date, password: password, letter: letter))
+                        }
+                    }
+                }
+            } catch {
+                
+            }
+        }
+        
+        return daysArr
+    }
+    
+    func writeJSON() -> Bool{
+        return false
+    }
+}
