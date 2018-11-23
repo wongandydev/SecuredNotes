cd041f9a4cf8a6bd72682d156134e8e31130364b
wongandydev <wongandy97@gmail.com>
2018-11-23 10:42:00 -0500

    remove readJSON() and add func to get current time in String format

--- GoingToJapan/Helper.swift
+++ GoingToJapan/Helper.swift
@@ -11,31 +11,6 @@ import UIKit
 
 class Helper {
     static let sharedInstance = Helper()
-    
-//    func readJSON() -> [DayPassObject]{
-//        var daysArr: [DayPassObject] = []
-//        
-//        if let filePath = Bundle.main.path(forResource: "fakeData", ofType: "json") {
-//            do {
-//                let json = try Data(contentsOf: URL(fileURLWithPath: filePath), options: .mappedIfSafe)
-//                let jsonResults = try JSONSerialization.jsonObject(with: json, options: .mutableLeaves)
-//                if let jsonResults = jsonResults as? [AnyObject] {
-//                    for result in jsonResults {
-//                        if let date = result["date"] as? String,
-//                            let password = result["password"] as? String,
-//                            let letter = result["note"] as? String{
-//                            daysArr.append(DayPassObject(date: date, password: password, letter: letter))
-//                        }
-//                    }
-//                }
-//            } catch {
-//                
-//            }
-//        }
-//        
-//        return daysArr
-//    }
-    
     func getCurrentTime() -> Date {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM/dd/yy' 'hh:mm:ss a"
